dcfade0b96650a39431123ce8b2995a4d8654b66
Khoa <lekhoa305@gmail.com>
2024-09-23 10:09:24 +0700

    Integrate ASP.NET Core Identity

---
+++ src/DevBlog.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevBlog.Core.Domain.Identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName { get; set; }
+    }
+}
--- src/DevBlog.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DevBlog.Data
-{
-    public class Class1
-    {
-
-    }
-}
